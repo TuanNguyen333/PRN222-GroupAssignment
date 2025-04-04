ce75516e281e8e8c40a394fe4dd21d385e760e67
vandatnguyen919 <vandatnguyen919@gmail.com>
2025-04-04 19:08:05 +0700

    Fix Logout function

--- eStore/Services/Common/StateContainer.cs
+++ eStore/Services/Common/StateContainer.cs
@@ -3,34 +3,33 @@ namespace eStore.Services.Common
 {
     public class StateContainer
     {
-        public string AuthToken { get; private set; }
-        public long ExpirationTime { get; private set; }
-        public bool IsAuthenticated => !string.IsNullOrEmpty(AuthToken);
+        public string AuthToken { get; set; }
+        public long ExpirationTime { get; set; }
+        public bool IsAuthenticated { get; private set; }
 
+        // Define the event for component notification
         public event Action OnChange;
 
         public void SetAuthData(string token, long expirationTime)
         {
-            Console.WriteLine($"Setting auth data. Token length: {token?.Length ?? 0}, ExpirationTime: {expirationTime}");
             AuthToken = token;
             ExpirationTime = expirationTime;
-            Console.WriteLine($"Auth data set. IsAuthenticated: {IsAuthenticated}");
+            IsAuthenticated = true;
             NotifyStateChanged();
         }
 
         public void ClearAuthData()
         {
-            Console.WriteLine("Clearing auth data");
             AuthToken = null;
             ExpirationTime = 0;
-            Console.WriteLine("Auth data cleared");
+            IsAuthenticated = false;
             NotifyStateChanged();
         }
 
         private void NotifyStateChanged()
         {
-            Console.WriteLine("Notifying state change");
             OnChange?.Invoke();
         }
     }
+
 }
